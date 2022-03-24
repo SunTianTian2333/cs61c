24e5805969390de50043a4e0f527bce58c246e97
SunTianTian2333 <2669516820@qq.com>
2022-03-24 02:26:31 -0700

    lab01 is done

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if(!head||!head->next)
+    return 0;
+    struct node* fast=head;
+    struct node* slow=head;
+    while(fast){
+    if(fast->next&&fast->next->next){
+    fast=fast->next->next;
+    }else return 0;
+    slow=slow->next;
+    if(slow==fast)
+    return 1;
+    }
+    
 }
